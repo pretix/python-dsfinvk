c1048730ba23277b68cacdab3c2175b1a822c628
Raphael Michel <michel@rami.io>
2024-08-24 15:06:13 +0200

    Allow to validate zip files

--- dsfinvk/validate.py
+++ dsfinvk/validate.py
@@ -3,6 +3,8 @@ import os
 import re
 import sys
 import xml.etree.ElementTree as ET
+import zipfile
+from io import StringIO
 
 
 class ValidationException(Exception):
@@ -35,7 +37,11 @@ def validate_files(filemap):
                 continue
 
             try:
-                validate_table(filemap[url], table)
+                if hasattr(filemap[url], 'read'):
+                    validate_table(filemap[url], table)
+                else:
+                    with open(filemap[url], encoding='utf-8') as f:
+                        validate_table(f, table)
             except ValidationException as e:
                 errors.append('{}: {}'.format(url, str(e)))
     return errors
@@ -64,64 +70,63 @@ def validate_table(f, table):
     if table.findall("./VariableLength/VariablePrimaryKey"):
         raise ValidationException("Primary key validation is currently not supported.")
 
-    with open(f, newline=record_delim, encoding='utf-8') as csvfile:
-        csvreader = csv.reader(
-            csvfile,
-            delimiter=column_delim,
-            quotechar=text_encaps,
-            quoting=csv.QUOTE_MINIMAL
-        )
-        coldefs = table.findall("./VariableLength/VariableColumn")
-        for i, row in enumerate(csvreader):
-            ncol_csv = len(row)
-            if ncol_csv != len(coldefs):
-                raise ValidationException("Line {}: Row has {} fields but index.xml defines {} fields.".format(
-                    i + 1, ncol_csv, len(coldefs)
-                ))
-            if i == 0:
-                for j, c in enumerate(coldefs):
-                    if c.find("./Name").text != row[j]:
-                        raise ValidationException("Expected column {} to be {}, but headline is {}.".format(
-                            j + 1, c.find("./Name").text, row[j]
-                        ))
-            else:
-                for j, c in enumerate(coldefs):
-                    if c.findall("./Numeric"):
-                        regex = regex_part_integer
-                        dec_places = 0
-                        if c.findall("./Numeric/Accuracy"):
-                            dec_places = int(c.find("./Numeric/Accuracy").text)
-                            regex += r"[%s]{1}[0-9]{%s}" % (dec_symb, dec_places)
-                        if row[j] == "":
-                            # It's unclear if empty strings are allowed in numeric fields
-                            continue
-                        if not re.match("^" + regex + "$", row[j]):
-                            print("^" + regex + "$", row[j])
+    csvreader = csv.reader(
+        f,
+        delimiter=column_delim,
+        quotechar=text_encaps,
+        quoting=csv.QUOTE_MINIMAL
+    )
+    coldefs = table.findall("./VariableLength/VariableColumn")
+    for i, row in enumerate(csvreader):
+        ncol_csv = len(row)
+        if ncol_csv != len(coldefs):
+            raise ValidationException("Line {}: Row has {} fields but index.xml defines {} fields.".format(
+                i + 1, ncol_csv, len(coldefs)
+            ))
+        if i == 0:
+            for j, c in enumerate(coldefs):
+                if c.find("./Name").text != row[j]:
+                    raise ValidationException("Expected column {} to be {}, but headline is {}.".format(
+                        j + 1, c.find("./Name").text, row[j]
+                    ))
+        else:
+            for j, c in enumerate(coldefs):
+                if c.findall("./Numeric"):
+                    regex = regex_part_integer
+                    dec_places = 0
+                    if c.findall("./Numeric/Accuracy"):
+                        dec_places = int(c.find("./Numeric/Accuracy").text)
+                        regex += r"[%s]{1}[0-9]{%s}" % (dec_symb, dec_places)
+                    if row[j] == "":
+                        # It's unclear if empty strings are allowed in numeric fields
+                        continue
+                    if not re.match("^" + regex + "$", row[j]):
+                        print("^" + regex + "$", row[j])
+                        print(
+                            "Line {}: Value {} in column {} is not a valid decimal with {} places".format(
+                                i + 1,
+                                row[j],
+                                j + 1,
+                                dec_places
+                            )
+                        )
+                elif c.findall("./AlphaNumeric"):
+                    if c.findall("./MaxLength"):
+                        ml = int(c.find("./MaxLength").text)
+                        if len(row[j]) > ml:
                             print(
-                                "Line {}: Value {} in column {} is not a valid decimal with {} places".format(
+                                "Line {}: Value {} in column {} is not allowed to have more than {} " \
+                                "characters".format(
                                     i + 1,
-                                    row[j],
+                                    repr(row[j]),
                                     j + 1,
-                                    dec_places
+                                    ml
                                 )
                             )
-                    elif c.findall("./AlphaNumeric"):
-                        if c.findall("./MaxLength"):
-                            ml = int(c.find("./MaxLength").text)
-                            if len(row[j]) > ml:
-                                print(
-                                    "Line {}: Value {} in column {} is not allowed to have more than {} " \
-                                    "characters".format(
-                                        i + 1,
-                                        repr(row[j]),
-                                        j + 1,
-                                        ml
-                                    )
-                                )
-                    elif c.findall("./Date"):
-                        raise ValidationException("Date validation currently not supported")
-                    else:
-                        raise ValidationException("Unsupported data type for column {}".format(j=j + 1))
+                elif c.findall("./Date"):
+                    raise ValidationException("Date validation currently not supported")
+                else:
+                    raise ValidationException("Unsupported data type for column {}".format(j=j + 1))
 
 
 def validate_dir(dirname):
@@ -129,6 +134,15 @@ def validate_dir(dirname):
     for f in os.listdir(dirname):
         fmap[f] = os.path.join(dirname, f)
     return validate_files(fmap)
+
+
+def validate_zip(archive):
+    with zipfile.ZipFile(archive, "r") as zf:
+        fmap = {}
+        for f in zf.namelist():
+            with zf.open(f) as fp:
+                fmap[f] = StringIO(fp.read().decode("utf-8"))
+        return validate_files(fmap)
 
 
 if __name__ == "__main__":
